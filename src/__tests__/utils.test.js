f4b4d08fede304e722ef643ce26d288536508f07
Christian Mugalo <christian.mugalo@student.moringaschool.com>
2024-06-08 20:56:07 +0300

    initial commit

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,11 @@
 // Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", () => {
+  it("returns true if the string is a palindrome, and false if it isn't", () => {
+    const wordOne = "racecar";
+    const palindromeCheck = isPalindrome(wordOne);
+
+    expect(palindromeCheck).toBe(true);
+  });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,9 @@
 // Your code here
+export function isPalindrome(str) {
+  const reversedStr = str.split("").reverse().join("");
+  if (str !== reversedStr) {
+    return false;
+  } else {
+    return true;
+  }
+}
