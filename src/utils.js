57139f253d789be24012ddcea555fabd710a5ef6
Christian Mugalo <christian.mugalo@student.moringaschool.com>
2024-06-08 21:13:35 +0300

    adds test code

--- src/utils.js
+++ src/utils.js
@@ -3,9 +3,7 @@ export function isPalindrome(str) {
   const convertedStr = str.toLowerCase();
   const reversedStr = convertedStr.split("").reverse().join("");
 
-  if (str === "") {
-    return false;
-  } else if (convertedStr !== reversedStr) {
+  if (str === "" || convertedStr !== reversedStr) {
     return false;
   } else {
     return true;
